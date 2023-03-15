324603a9a38522491f1e2b13cb73c6725ba6474c
Javier Serrano <jserranomolina@gmail.com>
2023-03-15 13:04:05 -0600

    About page added

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path("", views.homePageView, name="home"),
-    path("", views.index, name="index"),
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class HomePageView(TemplateView):
+    template_name = "home.html"
 
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class AboutPageView(TemplateView):
+    template_name = "about.html"
