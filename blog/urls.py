3395510f58cb1e91b20c2606c8bd578d5d3a1c30
Javier Serrano <jserranomolina@gmail.com>
2023-03-23 12:15:44 -0600

    CRUD blog post implementation

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailedView
+from .views import (
+    BlogListView,
+    BlogDetailedView,
+    BlogCreateView,
+    BlogEditView,
+    DeleteBlogView,
+)
 
 urlpatterns = [
+    path("post/<int:pk>/delete/", DeleteBlogView.as_view(), name="delete_post"),
+    path("post/<int:pk>/edit/", BlogEditView.as_view(), name="post_edit"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailedView.as_view(), name="post_detail"),
     path("", BlogListView.as_view(), name="blog"),
 ]
